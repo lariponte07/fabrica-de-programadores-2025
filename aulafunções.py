fd402ee044b84df68755ad7b60f8e3e7e5f4a24a
Larissa Ponte <larissaponte40@gmail.com>
2025-05-12 15:49:59 -0300

    Add files via upload

---
+++ aulafunções.py
@@ -0,0 +1,14 @@
+#saldo= int(input("Digite o saldo bancário "))
+#saque= int(input("Digite o valor de saque "))
+#if saldo>= saque:
+     #saldo=saldo-saque
+    #print("Você realizou um saque com sucesso. Seu saldo é",saldo)
+#else:
+    #print("Você não possui saldo suficiente para realizar essa operação.")
+nota1= int( input("Digite a primeira nota "))
+nota2= int( input("Digite a segunda nota "))
+media= (nota1+nota2)/2
+if media>=6:
+    print("Aprovado")
+else:
+    print("Reprovado")   
