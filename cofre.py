b2b252214b61f8e842b5912ad37488d51c7a6348
Larissa Ponte <larissaponte40@gmail.com>
2025-05-15 15:22:16 -0300

    Aula de "import" com professor Lázaro

---
+++ cofre.py
@@ -0,0 +1,29 @@
+#import senha
+
+#print(senha.minha_senha)
+
+#
+#import teste
+
+#print(teste.saudacao)
+
+#import random
+
+#numero_aleatorio= random.randint(0,10)
+#print(numero_aleatorio)
+
+#import random
+
+#compras= ['Chocolate', 'Leite','Morango','Bolacha','Suco']
+#print(random.choice(compras))
+
+#import time
+
+#print(time.asctime())
+
+#import turtle
+
+#turtle.circle(500)
+#turtle.getscreen()._root.mainloop()
+
+import this
