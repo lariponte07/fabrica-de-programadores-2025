26de768d1ce21ce8e505aa374bc71b691075485e
Larissa Ponte <larissaponte40@gmail.com>
2025-05-15 15:18:51 -0300

    Add files via upload
    Aula de números com professor Lázaro

---
+++ porcentagem.py
@@ -0,0 +1,19 @@
+#exercício 8 valor_total= float(input("Digite o valor total "))
+#porcentagem= 1.3
+#valor_parte=valor_total*(porcentagem/100)
+#print(valor_parte)
+#valor_final=valor_total+valor_parte
+#print("Saldo final: ", valor_final)
+
+#exercício 4 -
+# C= float(input("Digite a temperatura "))
+#farenheit =(9*C+160)/5
+#print("Temperatura: ",farenheit)
+
+num1=int(input("Digite um valor "))
+num2=int(input("Digite o seguundo valor "))
+if num1 > num2:
+    print('O resultado é: ',num1-num2)
+else:
+    print('O resultado é: ',num2-num1)
+    
