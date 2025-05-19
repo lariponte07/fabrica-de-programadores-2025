94ffe145612ca6ee89313e9c20d4e377731018a5
Larissa Ponte <larissaponte40@gmail.com>
2025-05-19 15:48:26 -0300

    Aula de equações

---
+++ equacao.py
@@ -0,0 +1,32 @@
+#ex1
+import math
+#def raizes_reais():
+#    a= float(input("Digite o quoeficiente..."))
+ #   b= float(input("Digite o quoeficiente..."))
+  #  c= float(input("Digite o quoeficiente..."))
+   # delta = b**2 - 4 * a * c
+    #if delta >=0:
+     #   raiz1 = (-b + math.sqrt(delta)) / (2*a)
+      #  raiz2 = (-b - math.sqrt(delta)) / (2*a)
+       # print("As raízes são", raiz1,raiz2 )
+    #else: 
+    #    print("Essa equação não possui raízes reais.")
+#raizes_reais()
+
+#ex2
+#numero_real1=float(input("Digite um número: "))
+#numero_real2=float(input("Digite um número: "))
+#valor_final=numero_real1-numero_real2 
+#print("A diferença entre os dois valores é:",valor_final)
+
+#ex3
+#raio_circunferencia=float(input("Digite o valor da circunferência: "))
+#comp_circunf= (2*math.pi*raio_circunferencia)
+#print("O comprimento da circunferência é:",comp_circunf)
+
+#ex4
+
+#ano_nasc=int(input("Digite o ano de nascimento:"))
+#ano_atual=int(input("Digite o ano atual:"))
+#idade_futura=ano_atual-ano_nasc+17
+#print("Sua idade daqui a 17 anos será:",idade_futura)
