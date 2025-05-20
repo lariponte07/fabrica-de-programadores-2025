45b06a44719d7c13e6c239c6597439cfe6b223aa
Larissa Ponte <larissaponte40@gmail.com>
2025-05-20 15:29:55 -0300

    Aula de "while"

---
+++ whileandfor.py
@@ -0,0 +1,25 @@
+#ex1
+#x=1
+#while x<+101:
+    #print(x)
+    #x = x + 1
+
+#ex2
+#x=50
+#while x<+101:
+    #print(x)
+    #x = x + 1
+
+#ex3
+#x=10
+#while x>=1:
+    #print(x)
+    #x = x - 1
+#print("Fogo!")
+
+fim=int(input("Digite o último número a imprimir: "))
+x=0
+while x <=fim:
+    if x % 2 == 0:
+        print(x)
+    x = x +1
