918db05ac84e437a01ee50c59a3879249d68be59
Larissa Ponte <larissaponte40@gmail.com>
2025-05-21 14:22:37 -0300

    Aula de while pt2

---
+++ while2.py
@@ -0,0 +1,15 @@
+#ex1
+#fim=int(input("Digite o último número a imprimir: "))
+#x= 0
+#while x <=fim:
+    #if x % 2 == 0:
+        #print(x)
+    #x = x + 1
+
+#ex2
+fim=int(input("Digite o último número a imprimir: "))
+x= 1
+while x >=fim:
+    if x % 1 == 0:
+        print(x)
+    x = x + 1
