2fb2298b33eb9d890c607b344d6cba857ef9faa6
Larissa Ponte <larissaponte40@gmail.com>
2025-05-27 15:48:48 -0300

    Aula de cálculos

---
+++ calculo.py
@@ -0,0 +1,36 @@
+#ex1
+#def soma_dos_quadrados(area):
+    #soma = quadrado(area) + quadrado(area)
+    #
+    # return soma 
+
+#def quadrado():
+    #lado=int(input("Digite o valor do lado: "))
+    #area=lado**2 # lado * lado
+    #print(area)
+#quadrado()
+
+#ex2
+#num1= float(input("Digite o número real "))
+#num2= float(input("Digite outro número real "))
+#diferenca= num1-num2
+#print("A diferença entre os dois números é:", diferenca)
+
+#ex3
+#import math
+#raio= float(input("Digite o raio da circunferência "))
+#comprimento= 2* math.pi * raio
+#print("O comprimento da circunferência é: ", comprimento)
+
+#ex4
+#anonasc= int(input("Digite o ano de nascimento: "))
+#anoatual= int(input("Digite o ano atual: "))
+#idade= anoatual- anonasc + 17
+#print("Em 17 anos, a idade será de:", idade, "anos")
+
+#ex5
+#quantidadekg= float(input("Digite a quantidade de alimento em Kg: "))
+#quantidadeg= quantidadekg * 1000
+#consumo_diario=50
+#dias= quantidadeg/ consumo_diario
+#print("A comida durará:",dias,"dias.")
